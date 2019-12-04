df4fa7900f2a0f392cc5cf41c8063930bf174c45
jeff-chan-sf-sei-05 <emailjeffchan@gmail.com>
2019-12-03 23:29:58 -0800

    login success

--- controllers/auth.js
+++ controllers/auth.js
@@ -41,6 +41,34 @@ const register = (req, res) => {
   });
 };
 
+
+// POST LOGIN - AUTHENTICATE USER, Create Session
+
+const login = (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).json({status: 400, message: "Please enter your email and password"});    
+  }
+  db.User.findOne({email: req.body.email}, (err, foundUser) => {
+    if (err) return res.status(500).json({status:500, message: 'Something went wrong. Please try again'});
+
+    if (!foundUser) {
+      return res.status(400).json({status: 400, message: "Email or password is incorrect"});
+    }
+    bcrypt.compare(req.body.password, foundUser.password, (err, isMatch) => {
+      if (err) return res.status(500).json({ status: 500, message: 'Somethign went wrong. Please try again'});
+
+      if (isMatch) {
+        req.session.currentUser = {id: foundUser._id};
+        console.log(req.session)
+        return res.status(200).json({ status: 200, message: 'Success', data: foundUser._id });
+      } else {
+        return res.status(400).json({ status: 400, message: 'Email or password is incorrect'});
+      }
+    });
+  });
+};
+
 module.exports = {
   register,
+  login,
 };
--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const ctrl = require ('../controllers');
 
 router.post('/register', ctrl.auth.register);
-// router.post('/login', ctrl.auth.login);
+router.post('/login', ctrl.auth.login);
 // router.get('/verify', ctrl.auth.verify);
 // router.delete('/logout', ctrl.auth.logout);
 
