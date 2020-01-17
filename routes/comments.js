2ad0b8aafe7dfec5809d68a157b626b9170ac659
jeff-chan-sf-sei-05 <emailjeffchan@gmail.com>
2020-01-17 10:50:50 -0800

    updated comment delete route - adding uesr and cast id

--- routes/comments.js
+++ routes/comments.js
@@ -5,7 +5,7 @@ const ctrl = require('../controllers');
 router.get("/:commentId", ctrl.comments.showOneComment),
 router.post("/:userId/:castId", ctrl.comments.createComment),
 router.put('/:commentId', ctrl.comments.updateComment),
-router.delete('/:commentId', ctrl.comments.destroy),
+router.delete('/:userId/:castId/:commentId', ctrl.comments.destroy),
 router.get("/cast/:castId", ctrl.comments.castComments),
 
 module.exports = router;
