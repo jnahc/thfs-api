0a88be440ef7869075e35aa64cca2ac45ce988ae
jeff-chan-sf-sei-05 <emailjeffchan@gmail.com>
2020-01-19 21:40:19 -0800

    adds show all comments route and controller v1

--- controllers/comments.js
+++ controllers/comments.js
@@ -161,6 +161,35 @@ const userComments = (req, res) => {
   });
 };
 
+// SHOW ALL COMMENTS
+
+const showAllComments = (req, res) => {
+  db.Comments.find({}, (err, allComments) => {
+    if (err) return res.status(500).json({
+      status: 500,
+      error: [{message: 'Something went wrong! Please try again'}],
+    });
+    // if (allComments) {
+    //   allComments.populate("author", "cast").execPopulate((err, comment) => {
+    //     if (err) return res.status(500).json({err})
+    //     res.send({
+    //       status: 200,
+    //       author: comment.author,
+    //       cast: comment.cast,
+    //     })
+    //   })
+    // } else {
+    //   res.status(500).json({message: 'Comment not found'})
+    // }
+    res.json({
+      status: 200,
+      count: allComments.length,
+      data: allComments,
+      requestedAt: new Date().toLocaleString(),
+    });
+  });
+};
+
 module.exports = {
   createComment,
   showOneComment,
@@ -168,4 +197,5 @@ module.exports = {
   destroy,
   castComments,
   userComments,
+  showAllComments,
 }
