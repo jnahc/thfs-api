fca9fdd6e4cf35f028e6909bca4007e737922445
jeff-chan-sf-sei-05 <emailjeffchan@gmail.com>
2020-01-19 21:50:13 -0800

    adds show all comments route and controller v2 - typo in routes

--- routes/comments.js
+++ routes/comments.js
@@ -6,8 +6,8 @@ router.get("/:commentId", ctrl.comments.showOneComment),
 router.post("/:userId/:castId", ctrl.comments.createComment),
 router.put('/:commentId', ctrl.comments.updateComment),
 router.delete('/:userId/:castId/:commentId', ctrl.comments.destroy),
-router.get("/cast/:castId", ctrl.comments.castComments),
+router.get('/cast/:castId', ctrl.comments.castComments),
 router.get("/all",
-ctrl.comments.destroy),
+ctrl.comments.showAllComments),
 
 module.exports = router;
